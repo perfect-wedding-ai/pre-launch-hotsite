63be1f41d4bddb1daa0f6088101c440c76de1e75
dyegomaas <dyego.maas@gmail.com>
2025-03-09 19:54:18 -0300

    Otimizações.

--- brides/src/app/head.tsx
+++ brides/src/app/head.tsx
@@ -2,11 +2,24 @@ export default function Head() {
   return (
     <>
       <link
+        rel="preload"
+        as="style"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+      <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossOrigin="anonymous"
         referrerPolicy="no-referrer"
+        media="print"
+        onLoad={(e) => {
+          const target = e.currentTarget;
+          target.media = 'all';
+        }}
       />
     </>
   )
